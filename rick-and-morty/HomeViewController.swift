8e057878e0841a7205670af451063a9a5a3377b6
Mertcan Bulut <winlentia@gmail.com>
2021-01-16 22:02:31 +0300

    project init, scene delegate removed

--- rick-and-morty/HomeViewController.swift
+++ rick-and-morty/HomeViewController.swift
@@ -7,10 +7,11 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class HomeViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        self.view.backgroundColor = .red
         // Do any additional setup after loading the view.
     }
 
